adcdd5afe818fc9ca650a4bbbf16696acf3f7430
alexmpDev <amparedes2004@gmail.com>
2024-11-18 16:44:19 +0100

    test service GetAllCategories done

--- src/test/java/com/alexmpdev/restaurantmanager/api/service/CategoriesServiceTest.java
+++ src/test/java/com/alexmpdev/restaurantmanager/api/service/CategoriesServiceTest.java
@@ -4,14 +4,16 @@ import com.alexmpdev.restaurantmanager.categories.model.Category;
 import com.alexmpdev.restaurantmanager.categories.repository.CategoryRepository;
 import com.alexmpdev.restaurantmanager.categories.service.impl.CategoryServiceImpl;
 import com.alexmpdev.restaurantmanager.common.BaseTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CategoriesServiceTest extends BaseTest {
@@ -29,7 +31,20 @@ public class CategoriesServiceTest extends BaseTest {
 
         categoryService.save(category);
 
-        verify(categoryRepository, times(1)).save(category);
+        verify(categoryRepository).save(category);
 
+    }
+
+    @Test
+    void CategoryService_GetAllCategories_returnList(){
+
+        List<Category> expectedCategories = List.of(getCategory("Prueba"));
+        when(categoryRepository.findAll()).thenReturn(expectedCategories);
+
+        List<Category> actualCategories = categoryService.getAllCategories();
+
+        verify(categoryRepository,atLeastOnce()).findAll();
+
+        Assertions.assertEquals(expectedCategories, actualCategories);
     }
 }
